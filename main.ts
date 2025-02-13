e89d239c498248b4d5defadff5542a28067e2c55
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-02-12 21:53:09 -0800

    Added healthz and metricsz handler.

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
+import express = require("express");
 import http = require("http");
+import promClient = require("prom-client");
 
 async function main() {
+  let app = express();
+  app.get("/", (req, res) => {
+    res.send("Hello World from express\n");
+  });
+  app.get("/healthz", (req, res) => {
+    res.end("OK");
+  });
+  app.get("/metricsz", async (req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.end(await promClient.register.metrics());
+  });
   http
-    .createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello World\n");
-    })
+    .createServer(app)
     .listen(8080, () => console.log("Listening on port 8080"));
   http
-    .createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello World from port 8081\n");
-    })
+    .createServer(app)
     .listen(8081, () => console.log("Listening on port 8081"));
 }
 

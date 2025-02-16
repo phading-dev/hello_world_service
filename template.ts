23fac7623ce94162edae3b04d9d6f143d13ad975
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-02-16 09:06:30 -0800

    Try use pre-built spanner_schema_update_cli.

--- template.ts
+++ template.ts
@@ -3,12 +3,8 @@ import { writeFileSync } from "fs";
 import "./environment";
 
 let cloudbuildTemplate = `steps:
-- name: 'node:22'
-  entrypoint: 'npm'
-  args: ['install']
-- name: 'node:22'
-  entrypoint: 'npx'
-  args: ['spanage', 'update', 'db/ddl', '-p', '${globalThis.PROJECT_ID}', '-i', '${globalThis.BALANCED_DB_INSTANCE_ID}', '-d', '${globalThis.DB_NAME}']
+- name: 'gcr.io/${globalThis.PROJECT_ID}/spanner_schema_update_cli'
+  args: ['update', 'db/ddl', '-p', '${globalThis.PROJECT_ID}', '-i', '${globalThis.BALANCED_DB_INSTANCE_ID}', '-d', '${globalThis.DB_NAME}']
 - name: 'gcr.io/cloud-builders/docker'
   args: ['build', '-t', 'gcr.io/${globalThis.PROJECT_ID}/hello-world-service:latest', '.']
 - name: "gcr.io/cloud-builders/docker"
